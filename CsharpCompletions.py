922105796a84065488329264f9ba7fa1e65720b4
Ofer Reichman <oferei@gmail.com>
2014-10-19 22:20:32 +0300

    expanded solution to all languages (JS & Boo) and enabled the priority change everywhere (inside comments/strings/constants as well.)

--- CsharpCompletions.py
+++ CsharpCompletions.py
@@ -2,8 +2,11 @@ import sublime, sublime_plugin
 
 class CsharpCompletions(sublime_plugin.EventListener):
 	def on_query_completions(self, view, prefix, locations):
-		if not view.match_selector(locations[0], 'source.cs -string -comment -constant'):
-			return []
+		loc = locations[0]
+		if not view.match_selector(loc, 'source.cs') and \
+			not view.match_selector(loc, 'source.js') and \
+			not view.match_selector(loc, 'source.boo'):
+				return []
 
 		compl_default = [view.extract_completions(prefix)]
 		compl_default = [(item + "\tDefault", item) for sublist in compl_default
